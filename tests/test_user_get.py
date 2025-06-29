b2043e080c1463da9ce8ebd87f3e28cd38792229
Jane Polenta <teran.kemuro@gmail.com>
2025-06-30 00:38:42 +0300

    ex16

--- tests/test_user_get.py
+++ tests/test_user_get.py
@@ -26,8 +26,42 @@ class TestUserGet(BaseCase):
         response2 = MyRequests.get(
             f"/user/{user_id_from_auth_method}",
             headers={'x-csrf-token': token},
-            cookies={"auth-sid": auth_sid}
+            cookies={"auth_sid": auth_sid}
         )
 
         expected_fields = ["username", "email", "firstName", "lastName"]
         Assertions.assert_json_has_keys(response2, expected_fields)
+
+# Ex16: Запрос данных другого пользователя
+    def test_get_another_user_data(self):
+        #Создание нового юзера
+        data = self.prepare_registration_data()
+        response1 = MyRequests.post("/user/", data=data)
+
+        email = data.get('email')
+        password = data.get('password')
+        data_1 = {
+            "email": email,
+            "password": password
+        }
+
+        #Авторизация под только что созданныv юзером
+        response2 = MyRequests.post("/user/login", data=data_1)
+        auth_sid = self.get_cookie(response2, "auth_sid")
+        token = self.get_header(response2, "x-csrf-token")
+
+        #Запрос данных юзера с id=2
+        response3 = MyRequests.get(
+            "/user/2",
+            headers={'x-csrf-token': token},
+            cookies={'auth_sid': auth_sid}
+        )
+
+        Assertions.assert_json_has_key(response3, "username")
+        Assertions.assert_json_has_not_key(response3, "email")
+        Assertions.assert_json_has_not_key(response3, "firstName")
+        Assertions.assert_json_has_not_key(response3, "lastName")
+
+
+
+
